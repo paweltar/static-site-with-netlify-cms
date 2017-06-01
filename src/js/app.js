e63410c1cc9c6cf0db4560cad7aad2c2f1808436
paweltar <paweltar@gmail.com>
2017-06-01 11:56:55 +0200

    Rebuild webpage using tachyons framework to include portfolio

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
 /* requires:
 vendor/jquery-3.2.0.js
+vendor/scrolloverflow.js
 vendor/jquery.fullpage.js
 */
+
+$(document).ready(function() {
+	$("#fullpage").fullpage({
+		paddingTop: "100px",
+		menu: "#menu",
+		anchors: ["home", "portfolio", "contact"],
+		scrollOverflow: true,
+		touchSensitivity: 15
+	});
+});
